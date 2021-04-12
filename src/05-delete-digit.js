c5d31d6796d6dcfdec6d3e4258888f2d68e0267b
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-04-12 17:54:00 +0300

    implement delete-digit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,22 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const numStr = n.toString();
+  const numArr = [];
+  for (let i = 0; i < numStr.length; i++) {
+    numArr.push(numStr.slice(0, i) + numStr.slice(i + 1, numStr.length));
+  }
+
+  let max = 0;
+  for (let i = 0; i < numArr.length; i++) {
+    const element = parseInt(numArr[i], 10);
+    if (max < element) {
+      max = element;
+    }
+  }
+
+  return max;
 }
 
 module.exports = deleteDigit;
