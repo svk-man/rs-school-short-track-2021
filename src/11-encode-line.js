7f87ed428837a8d1e3527870b1ad7c3e73440c92
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-04-12 20:11:24 +0300

    implement encode-line function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let count = 0;
+    const el = str.charAt(i);
+    for (let j = 0; j < str.length; j++) {
+      if (str.charAt(j) === el) {
+        count++;
+      }
+    }
+
+    if (!result.includes(str.charAt(i))) {
+      result += (count + str.charAt(i));
+    }
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
