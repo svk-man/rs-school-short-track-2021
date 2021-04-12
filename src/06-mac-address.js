da3ed3d5e11696547adbba4423a2ad6a24add64e
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-04-12 18:02:59 +0300

    implement mac-address function

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,8 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  return n.toString().search(new RegExp('([A-F0-9]{2}-){5}[A-F0-9]{2}')) !== -1;
 }
 
 module.exports = isMAC48Address;
